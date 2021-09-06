d28e381483f30bb305eaa80de23bf664dfd025b3
nemrosim <nemrosim1988@gmail.com>
2021-09-06 03:36:01 +0300

    fix env-s

--- packages/react/src/utils.ts
+++ packages/react/src/utils.ts
@@ -6,16 +6,18 @@ console.log('ENVs', process.env);
 /**
  * If your front is served on the same domain as your server, you can simply use:
  */
-export const socket = io(`${process.env.REACT_APP_IO_HOST}:${process.env.REACT_APP_IO_HOST_PORT}`);
+export const socket = io(
+    `${process.env.REACT_APP_WDS_SOCKET_HOST}:${process.env.REACT_APP_WDS_SOCKET_PORT}`,
+);
 
 const useCloudPeerServer = false;
 const generateUniqueId = true;
 
 const peerOptions = {
-    host: `${process.env.REACT_APP_PEER_HOST}`,
+    host: process.env.REACT_APP_PEER_HOST || 'http://localhost',
     port: process.env.REACT_APP_PEER_PORT ? parseInt(process.env.REACT_APP_PEER_PORT, 10) : 9000,
-    path: '/myapp',
-    secure: false, // need SSL
+    path: process.env.REACT_APP_PEER_PATH || '/myapp',
+    secure: Boolean(process.env.REACT_APP_PEER_SECURE), // need SSL
 };
 
 console.log('PEER OPTION', peerOptions);
