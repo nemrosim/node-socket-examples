0a3ec46e095be01c6c6f13b3aa9276670b3ec00d
nemrosim <nemrosim1988@gmail.com>
2021-09-06 03:26:36 +0300

    add env-s

--- packages/react/src/utils.ts
+++ packages/react/src/utils.ts
@@ -1,31 +1,24 @@
 import { io } from 'socket.io-client';
 import Peer from 'peerjs';
 
-console.log('NODE_ENV', process.env.NODE_ENV);
+console.log('ENVs', process.env);
 
 /**
  * If your front is served on the same domain as your server, you can simply use:
  */
-export const socket = io(
-    `${
-        process.env.NODE_ENV === 'production'
-            ? 'https://webrtc-server-node.herokuapp.com'
-            : 'http://localhost'
-    }:3001`,
-);
+export const socket = io(`${process.env.REACT_APP_IO_HOST}:${process.env.REACT_APP_IO_HOST_PORT}`);
 
 const useCloudPeerServer = false;
 const generateUniqueId = true;
 
 const peerOptions = {
-    host:
-        process.env.NODE_ENV === 'production'
-            ? 'https://webrtc-server-peerjs.herokuapp.com/'
-            : 'localhost',
-    port: 9000,
+    host: `${process.env.REACT_APP_PEER_HOST}`,
+    port: process.env.REACT_APP_PEER_PORT ? parseInt(process.env.REACT_APP_PEER_PORT, 10) : 9000,
     path: '/myapp',
     secure: false, // need SSL
 };
+
+console.log('PEER OPTION', peerOptions);
 
 /**
  * If you will not pass an "id" prop - peer js server will return you a unique ID
